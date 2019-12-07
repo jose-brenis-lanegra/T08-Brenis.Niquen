843765006f5c5ee4fd6fb5a1836b0727c228d76a
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-07 09:58:09 -0500

    Create longitud.py

---
+++ longitud.py
@@ -0,0 +1,74 @@
+#ejercicio01
+nombre="jose"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 4
+
+#ejercicio02
+nombre="antonio"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 7
+
+#ejercicio03
+nombre="remigio"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 7
+
+#ejercicio04
+nombre="brenis"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 6
+
+#ejercicio05
+nombre="lanegra"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 7
+
+#ejercicio06
+nombre="giancarlo"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 9
+
+#ejercicio07
+nombre="rene"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 4
+
+#ejercicio08
+nombre="martin"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 6
+
+#ejercicio09
+nombre="eduardo"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 7
+
+#ejercicio10
+nombre="lida"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 4
+
+#ejercicio11
+nombre="del rosario"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 11
+
+#ejercicio12
+nombre="antero"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 6
+
+#ejercicio13
+nombre="thalia"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 6
+
+#ejercicio14
+nombre="francisco"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 9
+
+#ejercicio15
+nombre="valentino"
+long=len(nombre)
+print("la longitud del nombre es:",long)   #mostrar 9
