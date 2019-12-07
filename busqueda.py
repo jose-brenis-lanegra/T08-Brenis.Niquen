20cc990473587596e2bee972bcae8ce78a76f9d7
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-07 16:08:24 -0500

    Create busqueda.py

---
+++ busqueda.py
@@ -0,0 +1,89 @@
+#ejercicio01
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("v"))   #mostrar 22
+
+#ejercicio02
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("U"))   #mostrar 46
+
+#ejercicio03
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("t"))   #mostrar 4
+
+#ejercicio04
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("S"))   #mostrar 44
+
+#ejercicio05
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("k"))   #mostrar 17
+
+#ejercicio06
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("X"))   #mostrar 49
+
+#ejercicio07
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("d"))   #mostrar 12
+
+#ejercicio08
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.find("+"))   #mostrar -1
+
+#ejercicio09
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("G"))   #mostrar 32
+
+#ejercicio10
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("w"))   #mostrar 1
+
+#ejercicio11
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("O"))   #mostrar 40
+
+#ejercicio12
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("u"))   #mostrar 6
+
+#ejercicio13
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("Z"))   #mostrar 51
+
+#ejercicio14
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("y"))   #mostrar 5
+
+#ejercicio15
+#    0         10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+msg="qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(msg.index("M"))   #mostrar 38
