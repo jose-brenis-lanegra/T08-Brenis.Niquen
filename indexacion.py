09bcd65ac6fb391a81df27656bc6583402a76e80
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-06 23:01:29 -0500

    Create indexacion.py

---
+++ indexacion.py
@@ -0,0 +1,89 @@
+#ejercicio01
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[37])    #mostrar "a"
+
+#ejercicio02
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[22])    #mostrar "n"
+
+#ejercicio03
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[18])    #mostrar "0"
+
+#ejercicio04
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[4])    #mostrar "t"
+
+#ejercicio05
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[17])    #mostrar "g"
+
+#ejercicio06
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[34])    #mostrar "p"
+
+#ejercicio07
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[26])    #mostrar "e"
+
+#ejercicio08
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[7])    #mostrar "s"
+
+#ejercicio09
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[19])    #mostrar "b"
+
+#ejercicio10
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[41])    #mostrar "d"
+
+#ejercicio11
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[25])    #mostrar "l"
+
+#ejercicio12
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[15])    #mostrar "i"
+
+#ejercicio13
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[5])    #mostrar "r"
+
+#ejercicio14
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[10])    #mostrar " "
+
+#ejercicio15
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012
+msg="nosotros a los ingobernables , no pasa nada"
+print(msg[29])    #mostrar ","
