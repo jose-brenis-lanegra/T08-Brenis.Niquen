88e8c064cd6df7c0e953e15894c10e969bf003e9
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-07 15:54:59 -0500

    Create iteracion.py

---
+++ iteracion.py
@@ -0,0 +1,89 @@
+#ejercicio01
+str="jose"
+for letra in str:
+    print(letra)    #mostrar jose en forma vertical
+
+#ejercicio02
+str="antonio"
+for letra in str:
+    print(letra)    #mostrar antonio en forma vertical
+
+#ejercicio03
+str="remigio"
+for letra in str:
+    print(letra)    #mostrar remigio en forma vertical
+
+#ejercicio04
+str="brenis"
+for letra in str:
+    print(letra)    #mostrar brenis en forma vertical
+
+#ejercicio05
+str="lanegra"
+for letra in str:
+    print(letra)    #mostrar lanegra en forma vertical
+
+#ejercicio06
+str="giancarlo"
+for letra in str:
+    print(letra)    #mostrar giancarlo en forma vertical
+
+#ejercicio07
+str="rene"
+for letra in str:
+    print(letra)    #mostrar rene en forma vertical
+
+#ejercicio08
+str="martin"
+letra_m=0
+for letra in str:
+    if (letra=="m"):
+        letra_m+=1
+print("la cantidad de m es:", letra_m)   #mostrar la cantidad de m
+
+#ejercicio09
+str="eduardo"
+letra_d=0
+for letra in str:
+    if (letra=="d"):
+        letra_d+=1
+print("la cantidad de d es:", letra_d)   #mostrar la cantidad de d
+
+#ejercicio10
+str="lida"
+letra_a=0
+for letra in str:
+    if (letra=="a"):
+        letra_a+=1
+print("la cantidad de m es:", letra_a)   #mostrar la cantidad de a
+
+#ejercicio11
+str="del rosario"
+letra_r=0
+for letra in str:
+    if (letra=="r"):
+        letra_r+=1
+print("la cantidad de r es:", letra_r)   #mostrar la cantidad de r
+
+#ejercicio12
+str="antero"
+letra_t=0
+for letra in str:
+    if (letra=="t"):
+        letra_t+=1
+print("la cantidad de t es:", letra_t)   #mostrar la cantidad de t
+
+#ejercicio13
+str="thalia"
+for letra in str:
+    print(letra)    #mostrar thalia en forma vertical
+
+#ejercicio14
+str="francisco"
+for letra in str:
+    print(letra)    #mostrar francisco en forma vertical
+
+#ejercicio15
+str="valentino"
+for letra in str:
+    print(letra)    #mostrar valentino en forma vertical
