97abbf638b5926523537406821ae2fb808b30aa9
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-07 12:17:00 -0500

    Create cortado.py

---
+++ cortado.py
@@ -0,0 +1,89 @@
+#ejercicio01
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[0:2])     #mostrar HO
+
+#ejercicio02
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[0:4])     #mostrar HOLA
+
+#ejercicio03
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[0:6])     #mostrar HOLA U
+
+#ejercicio04
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[8:19])     #mostrar GUSTO VERTE
+
+#ejercicio05
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[:12])     #mostrar HOLA UN GUST
+
+#ejercicio06
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[:])     #mostrar HOLA UN GUSTO VERTE
+
+#ejercicio07
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[::-1])     #mostrar ETREV OTSUG NU ALOH
+
+#ejercicio08
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[19::-1])     #mostrar ETREV OTSUG NU ALOH
+
+#ejercicio09
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[::-2])     #mostrar ERVOSGN LH
+
+#ejercicio10
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[::2])     #mostrar HL NGSOVRE
+
+#ejercicio11
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[19:8:-1])     #mostrar ETREV OTSU
+
+#ejercicio12
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[:8:-1])     #mostrar ETREV OTSU
+
+#ejercicio13
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[:10])     #mostrar HOLA UN GU
+
+#ejercicio14
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[10::-1])     #mostrar SUG NU ALOH
+
+#ejercicio15
+#    0         10
+#    0123456789012345678
+str="HOLA UN GUSTO VERTE"
+print(str[5:10])     #mostrar UN GU
