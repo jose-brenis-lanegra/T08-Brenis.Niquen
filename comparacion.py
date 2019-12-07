ec89c9ec1e58c078df67bdd75af6db9c1dc8c7de
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-07 10:47:46 -0500

    Create comparacion.py

---
+++ comparacion.py
@@ -0,0 +1,95 @@
+#ejercicio01
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("f"=="3")         #mostrar false
+
+#ejercicio02
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("l"!="r")         #mostrar true
+
+#ejercicio03
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("8">"h")         #mostrar true
+
+#ejercicio04
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("j"<"q")         #mostrar false
+
+#ejercicio05
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("7">="c")         #mostrar true
+
+#ejercicio06
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("d"<="d")         #mostrar true
+
+#ejercicio07
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("y"=="g")         #mostrar false
+
+#ejercicio07
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("x"!="0")         #mostrar true
+
+#ejercicio08
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("z">="w")         #mostrar true
+
+#ejercicio09
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("h"<="2")         #mostrar true
+
+#ejercicio10
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("s">"m")         #mostrar false
+
+#ejercicio11
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("i"<"p")         #mostrar true
+
+#ejercicio12
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("v"=="7")         #mostrar false
+
+#ejercicio13
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("k"!="n")         #mostrar true
+
+#ejercicio14
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("g"<"e")         #mostrar false
+
+#ejercicio15
+#    0         10        20        30
+#    012345678901234567890123456789012345
+msg="qwertyuiopasdfghjklzxcvbnm1234567890"
+print("b"<="b")         #mostrar true
